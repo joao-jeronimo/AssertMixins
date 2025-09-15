e91107a57fa5a7646ae521c08573a52b363b4531
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-09-15 20:22:21 +0100

    Esqueleto teste.

---
+++ AssertMixins/assert_mixins/testcases/test_elementary.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import unittest
+from unittest.mock import Mock, MagicMock
+
+class TestElementaryMixin(unittest.TestCase):
+    """
+    Test behaviour of class ElementaryMixin.
+    """
+    
+    def test_skel(self):
+        """
+        Verified correct creation of testing data.
+        """
+        self.assertTrue(True)
+    
+    ############################################################
+    ############################################################
+    ############################################################
+    @classmethod
+    def setUpClass(self):
+        pass
