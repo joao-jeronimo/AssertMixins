275f61251759f5410dea0c00b787bcd8f0a777b3
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-09-15 20:35:18 +0100

    Script de rotinas.

---
+++ Routines.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -e -x
+
+SENSIBLE_TIMEOUT=$((5*60))
+
+case $1 in
+  deps_devel)
+    pushd AssertMixins/
+    python3 -m pip install --upgrade -r requirements.txt
+    popd
+    ;;
+    
+  test_unit)
+    pushd AssertMixins/
+    pytest --timeout=$SENSIBLE_TIMEOUT
+    popd
+    ;;
+	
+  full_local_test)
+    # Clear the screen:
+    reset; clear
+    # Add modifications, update editable installations and run tests, in order:
+    git add .
+    pushd AssertMixins/
+    pip install -e .
+    pytest --timeout=$SENSIBLE_TIMEOUT
+    # Always return to oeiginal directory:
+    popd
+    ;;
+	
+  *)
+    echo "Unknown routine name: $1"
+    exit -1
+    ;;
+esac
