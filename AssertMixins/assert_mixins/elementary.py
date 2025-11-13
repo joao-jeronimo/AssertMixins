961192a71e6e846be849134bd77863721d944174
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-11-13 11:20:55 +0000

    O esqueleto.

---
+++ AssertMixins/assert_mixins/elementary.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class ElementaryMixin:
+    """
+    Elementary assertions, meaning:
+     * Based on standard unittest assertions;
+     * Or delivering better reporting;
+     * Or with more generality.
+    """
+    pass
